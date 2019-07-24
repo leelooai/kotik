608fa8b234cad20fbc3a521ba927d0d223e52b62
Andriy Lupa <lupa_andriy@list.ru>
2019-07-24 09:58:26 +0300

    Updates

---
+++ precache-manifest.adad913129f1087957cad8d785520caf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31137b297b41d235db850ec1ac54499",
+    "url": "/get_fb_tokens/index.html"
+  },
+  {
+    "revision": "3f050de1a5884264f68e",
+    "url": "/get_fb_tokens/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/get_fb_tokens/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3f050de1a5884264f68e",
+    "url": "/get_fb_tokens/static/js/main.443a5203.chunk.js"
+  },
+  {
+    "revision": "cfa272e05375e8555946",
+    "url": "/get_fb_tokens/static/js/runtime~main.93eafbd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/get_fb_tokens/static/media/logo.5d5d9eef.svg"
+  }
+]);
